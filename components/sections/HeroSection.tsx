15d3c604c5b19a924f57d7df9e8738640d6785a0
SantiZai <santyagozaidan@gmail.com>
2024-05-09 13:41:35 -0300

    changes in the hero spacing

--- components/sections/HeroSection.tsx
+++ components/sections/HeroSection.tsx
@@ -1,6 +1,8 @@
+import { Button } from "../ui/button";
+
 export default function HeroSection() {
   return (
-    <section>
+    <section className="h-screen w-11/12 mx-auto flex items-center">
       <div className="w-2/3 flex flex-col gap-2">
         <h1 className="text-5xl text-balance">
           ¡Armá tu próximo partido al instante!
@@ -9,6 +11,7 @@ export default function HeroSection() {
           Navegá entre todos los clubes disponibles y reservá tu cancha en
           tiempo real.
         </h3>
+        <Button className="w-1/3">Buscar cancha</Button>
       </div>
     </section>
   );
