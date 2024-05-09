528bf624b0e45e223bc3b2ac4aadfad8fae4f0ab
SantiZai <santyagozaidan@gmail.com>
2024-05-09 16:15:23 -0300

    differentials

--- components/pures/Differential.tsx
+++ components/pures/Differential.tsx
@@ -1,3 +1,39 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+const differentials = [
+  {
+    text: "¡Navegá entre cientos de clubes!",
+  },
+  {
+    text: "Disponibilidad de canchas en ",
+    boldText: "tiempo real",
+  },
+];
+
 export default function Differential() {
-    
+  const [diff, setDiff] = useState(0);
+
+  const handleDiffChange = () => {
+    setDiff((prevIndex) =>
+      prevIndex === differentials.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
+  const { text, boldText } = differentials[diff];
+
+  useEffect(() => {
+    const interval = setInterval(handleDiffChange, 4000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <section className="w-full bg-gray-200 py-8 text-center">
+      <h3 className="text-2xl">
+        {text}
+        {boldText && <strong>{boldText}</strong>}
+      </h3>
+    </section>
+  );
 }
