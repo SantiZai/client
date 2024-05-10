693b56442846ea2d46bc57e47c4427a37101e512
SantiZai <santyagozaidan@gmail.com>
2024-05-10 11:55:23 -0300

    fix the width of the link

--- components/sections/HeroSection.tsx
+++ components/sections/HeroSection.tsx
@@ -3,7 +3,7 @@ import { Button } from "../ui/button";
 export default function HeroSection() {
   return (
     <section className="h-screen w-11/12 mx-auto flex items-center">
-      <div className="w-2/3 flex flex-col gap-2">
+      <div className="w-full sm:w-2/3 flex flex-col gap-4 sm:gap-2">
         <h1 className="text-5xl text-balance">
           ¡Armá tu próximo partido al instante!
         </h1>
@@ -11,9 +11,9 @@ export default function HeroSection() {
           Navegá entre todos los clubes disponibles y reservá tu cancha en
           tiempo real.
         </h3>
-        <a href="#search">
-          <Button className="w-1/3">Buscar cancha</Button>
-        </a>
+        <Button size={"lg"} className="w-full sm:w-1/4">
+          <a href="#search">Buscar cancha</a>
+        </Button>
       </div>
     </section>
   );
