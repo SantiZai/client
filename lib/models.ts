aad7a4a08747b77cfb6f3fa771fdc7eb45dab19a
SantiZai <santyagozaidan@gmail.com>
2024-05-13 14:19:17 -0300

    create the models

---
+++ lib/models.ts
@@ -0,0 +1,62 @@
+enum USER_TYPES {
+  superadmin,
+  admin,
+  user,
+}
+
+enum SPORTS {
+  tennis,
+  soccer,
+  basketball,
+  volley,
+  rugby,
+}
+
+enum SURFACES {
+  clay,
+  grass,
+  hard,
+}
+
+interface Club {
+  id: string;
+  createdAt: Date;
+  name: string;
+  location: string;
+  courts: Court[];
+  admin: User;
+  adminId: string;
+  sports: SPORTS[];
+}
+
+interface Court {
+  id: string;
+  createdAt: Date;
+  name: string;
+  surface: SURFACES;
+  club: Club;
+  clubId: string;
+  reservations: Reservation[];
+  sport: SPORTS;
+}
+
+interface Reservation {
+  id: string;
+  createdAt: Date;
+  date: string;
+  hour: string;
+  court: Court;
+  courtId: string;
+  user: User;
+  userId: string;
+}
+
+interface User {
+  id: string;
+  createdAt: Date;
+  fullname: string;
+  email: string;
+  reservations: Reservation[];
+  userType: USER_TYPES;
+  club?: Club;
+}
