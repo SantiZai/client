31300fc66e75ac19c6047bd1f62c5b541815aea1
SantiZai <santyagozaidan@gmail.com>
2024-05-15 20:15:18 -0300

    display the clubs filtereds

--- components/sections/FilteredClubs.tsx
+++ components/sections/FilteredClubs.tsx
@@ -1,41 +1,30 @@
-"use client";
-
 import { Club } from "@/lib/models";
-import { useState, useEffect } from "react";
 import Image from "next/image";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLocationDot, faStar } from "@fortawesome/free-solid-svg-icons";
 
 export default function FilteredClubs({
   searchedClubs,
-  filters,
 }: {
   searchedClubs: Club[];
-  filters?: any;
 }) {
-  const [clubs, setClubs] = useState<Club[]>([]);
-
-  useEffect(() => {
-    setClubs(searchedClubs);
-  }, [filters]);
   return (
-    <section className="w-11/12 mx-auto">
-      <h3 className="mb-4">Filtered clubs</h3>
-      <ul className="flex gap-6">
-        {searchedClubs.map((club: Club) => (
-          <li className="w-full sm:w-1/2 md:w-1/4 p-2 rounded-lg hover:shadow-lg transition-all duration-500">
+    <section className="w-11/12 mx-auto mt-8">
+      <ul className="flex flex-wrap gap-6">
+        {searchedClubs.length > 0 && searchedClubs.map((club: Club) => (
+          <li className="w-full sm:w-1/2 lg:w-1/3 xl:1/4 p-2 rounded-lg hover:shadow-lg transition-all duration-500">
             <div>
               <Image
                 src={club.image}
                 alt={`${club.name} profile picture`}
                 height={100}
                 width={500}
-                className="rounded-lg"
+                className="rounded-lg w-full"
               />
             </div>
-            <div className="mt-2 flex justify-between">
+            <div className="mt-2 flex justify-between p-2">
               <div className="flex flex-col gap-2">
-                <h5 className="text-lg hover:cursor-pointer hover:text-[1.2rem] transition-all">
+                <h5 className="text-lg hover:cursor-pointer hover:text-[1.15rem] transition-all">
                   {club.name
                     .split("-")
                     .map((each) => each[0].toUpperCase() + each.slice(1))
