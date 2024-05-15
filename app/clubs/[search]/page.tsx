a4d595b97dbe250d40b65901f46bcf8c80292e1f
SantiZai <santyagozaidan@gmail.com>
2024-05-15 20:14:51 -0300

    states for filters

--- app/clubs/[search]/page.tsx
+++ app/clubs/[search]/page.tsx
@@ -2,8 +2,8 @@
 
 import FilterClubs from "@/components/sections/FilterCLubs";
 import FilteredClubs from "@/components/sections/FilteredClubs";
-import { getClubsByLocation } from "@/lib/data";
-import { Club } from "@/lib/models";
+import { getClubsByLocation, getClubsByLocationAndSport } from "@/lib/data";
+import { Club, SPORTS } from "@/lib/models";
 import { useEffect, useState } from "react";
 
 export default function FindedClubs({
@@ -12,13 +12,28 @@ export default function FindedClubs({
   params: { search: string };
 }) {
   const [clubs, setClubs] = useState<Club[]>([]);
+  const [location, setLocation] = useState<string>(params.search);
+  const [sport, setSport] = useState<SPORTS>(SPORTS.tennis);
+  const [date, setDate] = useState<Date>();
+  const [hour, setHour] = useState<string>();
 
   useEffect(() => {
     getClubsByLocation(params.search).then((res) => setClubs(res));
   }, [params.search]);
 
-  return (<main className="h-screen mt-24">
-    <FilterClubs />
-    <FilteredClubs searchedClubs={clubs} />
-  </main>)
+  useEffect(() => {
+    getClubsByLocationAndSport(location, sport).then((res) => setClubs(res));
+  }, [location, sport, date, hour]);
+
+  return (
+    <main className="mt-24">
+      <FilterClubs
+        setLocation={setLocation}
+        setSport={setSport}
+        date={date}
+        setDate={setDate}
+      />
+      <FilteredClubs searchedClubs={clubs} />
+    </main>
+  );
 }
