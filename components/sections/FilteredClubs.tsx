041d83a1f7193cbcf74618706e3ddcfcd1dd6f61
SantiZai <santyagozaidan@gmail.com>
2024-05-15 12:24:40 -0300

    most changes in the clubs cards

--- components/sections/FilteredClubs.tsx
+++ components/sections/FilteredClubs.tsx
@@ -23,7 +23,7 @@ export default function FilteredClubs({
       <h3 className="mb-4">Filtered clubs</h3>
       <ul className="flex gap-6">
         {searchedClubs.map((club: Club) => (
-          <li className="w-full sm:w-1/2 md:w-1/4 hover:cursor-pointer hover:scale-105 transition-all duration-300">
+          <li className="w-full sm:w-1/2 md:w-1/4 p-2 rounded-lg hover:shadow-lg transition-all duration-500">
             <div>
               <Image
                 src={club.image}
@@ -35,14 +35,18 @@ export default function FilteredClubs({
             </div>
             <div className="mt-2 flex justify-between">
               <div className="flex flex-col gap-2">
-                <h5 className="text-lg">
+                <h5 className="text-lg hover:cursor-pointer hover:text-[1.2rem] transition-all">
                   {club.name
                     .split("-")
                     .map((each) => each[0].toUpperCase() + each.slice(1))
                     .join(" ")}
                 </h5>
                 <div className="flex gap-2 items-center">
-                  <FontAwesomeIcon icon={faLocationDot} width={30} className="w-full" />
+                  <FontAwesomeIcon
+                    icon={faLocationDot}
+                    width={30}
+                    className="w-full"
+                  />
                   <div className="flex flex-col">
                     <span className="text-xs">{club.address}</span>
                     <span className="text-xs">{club.location}</span>
@@ -50,9 +54,23 @@ export default function FilteredClubs({
                 </div>
               </div>
               <div className="flex flex-col justify-between items-end">
-                <span>5 <FontAwesomeIcon icon={faStar} /></span>
+                <span>
+                  5 <FontAwesomeIcon icon={faStar} />
+                </span>
                 <span>$1000</span>
               </div>
+            </div>
+            {/* TODO: agregar la funcionalidad para traer los horarios disponibles en cada club */}
+            <div className="flex gap-2 mt-4">
+              <span className="px-4 py-2 border border-slate-200 rounded-md hover:cursor-pointer hover:bg-slate-200 transition-all">
+                19:00
+              </span>
+              <span className="px-4 py-2 border border-slate-200 rounded-md hover:cursor-pointer hover:bg-slate-200 transition-all">
+                19:30
+              </span>
+              <span className="px-4 py-2 border border-slate-200 rounded-md hover:cursor-pointer hover:bg-slate-200 transition-all">
+                20:00
+              </span>
             </div>
           </li>
         ))}
