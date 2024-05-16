413aac4132868f2f856934fb672d9a075700406e
SantiZai <santyagozaidan@gmail.com>
2024-05-16 11:26:01 -0300

    map title

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,12 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
+export const cn = (...inputs: ClassValue[]) => {
+  return twMerge(clsx(inputs));
+};
+
+export const mapClubTitle = (title: string) =>
+  title
+    .split("-")
+    .map((each) => each[0].toUpperCase() + each.slice(1))
+    .join(" ");
