2c595432bb2b770bcf246626b3a8731200235c87
SantiZai <santyagozaidan@gmail.com>
2024-06-10 18:12:48 -0300

    block hours if not exists an available court

--- components/pures/InfiniteHorizontalScroll.tsx
+++ components/pures/InfiniteHorizontalScroll.tsx
@@ -1,3 +1,4 @@
+import { generateAllHours } from "@/lib/manageReservationHours";
 import { Button } from "../ui/button";
 
 const InfiniteHorizontalScroll = ({
@@ -7,15 +8,14 @@ const InfiniteHorizontalScroll = ({
   hours: string[];
   setSelectedHour: (hour: string) => void;
 }) => {
-  //TODO: block hours if not exists available courts
   return (
     <div
       className="w-11/12 mx-auto overflow-scroll whitespace-nowrap"
       style={{ scrollbarWidth: "none" }}
     >
-      <div className="inline-flex">
-        {hours.map((hour: string, index: number) =>
-          hours[index] == hours[0] ? (
+      <div className="inline-flex gap-2">
+        {generateAllHours().map((hour: string, index: number) =>
+          hours.includes(hour) ? (
             <Button
               key={index}
               variant={"outline"}
@@ -24,21 +24,12 @@ const InfiniteHorizontalScroll = ({
             >
               {hour}
             </Button>
-          ) : hours[index] == hours[hours.length - 1] ? (
-            <Button
-              key={index}
-              variant={"outline"}
-              onClick={() => setSelectedHour(hour)}
-              className="p-6 rounded-3xl border-slate-500 font-bold ml-2"
-            >
-              {hour}
-            </Button>
           ) : (
             <Button
               key={index}
               variant={"outline"}
-              onClick={() => setSelectedHour(hour)}
-              className="p-6 rounded-3xl border-slate-500 font-bold mx-2"
+              disabled
+              className="p-6 rounded-3xl border-slate-500 font-bold mr-2"
             >
               {hour}
             </Button>
