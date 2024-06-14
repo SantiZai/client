91cf52a75f938eb40b330b732686f1f9a3613880
SantiZai <santyagozaidan@gmail.com>
2024-06-14 12:42:54 -0300

    block the hours if the formatted date doesnt exists

--- components/pures/InfiniteHorizontalScroll.tsx
+++ components/pures/InfiniteHorizontalScroll.tsx
@@ -4,9 +4,11 @@ import { Button } from "../ui/button";
 const InfiniteHorizontalScroll = ({
   hours,
   setSelectedHour,
+  blocked,
 }: {
   hours: string[];
   setSelectedHour: (hour: string) => void;
+  blocked: boolean;
 }) => {
   return (
     <div
@@ -14,27 +16,38 @@ const InfiniteHorizontalScroll = ({
       style={{ scrollbarWidth: "none" }}
     >
       <div className="inline-flex gap-2">
-        {generateAllHours().map((hour: string, index: number) =>
-          hours.includes(hour) ? (
-            <Button
-              key={index}
-              variant={"outline"}
-              onClick={() => setSelectedHour(hour)}
-              className="p-6 rounded-3xl border-slate-500 font-bold mr-2"
-            >
-              {hour}
-            </Button>
-          ) : (
-            <Button
-              key={index}
-              variant={"outline"}
-              disabled
-              className="p-6 rounded-3xl border-slate-500 font-bold mr-2"
-            >
-              {hour}
-            </Button>
-          )
-        )}
+        {blocked
+          ? generateAllHours().map((hour: string, index: number) => (
+              <Button
+                key={index}
+                variant={"outline"}
+                disabled
+                className="p-6 rounded-3xl border-slate-500 font-bold mr-2"
+              >
+                {hour}
+              </Button>
+            ))
+          : generateAllHours().map((hour: string, index: number) =>
+              hours.includes(hour) ? (
+                <Button
+                  key={index}
+                  variant={"outline"}
+                  onClick={() => setSelectedHour(hour)}
+                  className="p-6 rounded-3xl border-slate-500 font-bold mr-2"
+                >
+                  {hour}
+                </Button>
+              ) : (
+                <Button
+                  key={index}
+                  variant={"outline"}
+                  disabled
+                  className="p-6 rounded-3xl border-slate-500 font-bold mr-2"
+                >
+                  {hour}
+                </Button>
+              )
+            )}
       </div>
     </div>
   );
