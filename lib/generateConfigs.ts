fdd58c5c7b9279993d521d1ccc107e8c2bffb4f4
SantiZai <santyagozaidan@gmail.com>
2024-11-22 17:54:41 -0300

    delete config

--- lib/generateConfigs.ts
+++ lib/generateConfigs.ts
@@ -1,18 +1,27 @@
 export const GET_CONFIG = () => {
   return {
-    method: "GET",
+    method: 'GET',
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
   };
 };
 
 export const POST_CONFIG = (props: any) => {
   return {
-    method: "POST",
+    method: 'POST',
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify({ ...props }),
+  };
+};
+
+export const DELETE_CONFIG = () => {
+  return {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
   };
 };
