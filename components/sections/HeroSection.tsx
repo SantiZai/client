2189743021ec9e965ad4c675110244970ba69dbb
SantiZai <santyagozaidan@gmail.com>
2024-11-22 18:53:39 -0300

    change the colors

--- components/sections/HeroSection.tsx
+++ components/sections/HeroSection.tsx
@@ -1,22 +1,24 @@
-import { Button } from "../ui/button";
+import { Button } from '../ui/button';
 
 const HeroSection = () => {
   return (
-    <section className="h-screen w-11/12 mx-auto flex items-center">
-      <div className="w-full sm:w-2/3 flex flex-col gap-4 sm:gap-2">
-        <h1 className="text-5xl text-balance">
-          ¡Armá tu próximo partido al instante!
-        </h1>
-        <h3 className="text-xl text-balance">
-          Navegá entre todos los clubes disponibles y reservá tu cancha en
-          tiempo real.
-        </h3>
-        <Button
-          size={"lg"}
-          className="w-full sm:w-1/4"
-        >
-          <a href="#search">Buscar cancha</a>
-        </Button>
+    <section className="h-screen w-full bg-center bg-cover bg-no-repeat">
+      <div className="h-screen w-11/12 mx-auto flex items-center">
+        <div className="w-full sm:w-2/3 flex flex-col gap-4 sm:gap-2">
+          <h1 className="text-5xl text-balance">
+            ¡Armá tu próximo partido al instante!
+          </h1>
+          <h3 className="text-xl text-balance">
+            Navegá entre todos los clubes disponibles y reservá tu cancha en
+            tiempo real.
+          </h3>
+          <Button
+            size={'lg'}
+            className="w-full sm:w-1/4"
+          >
+            <a href="#search">Buscar cancha</a>
+          </Button>
+        </div>
       </div>
     </section>
   );
