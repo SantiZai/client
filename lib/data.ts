c2585fc6bf96dc44d2c54a17ff3916bdaa2c7814
SantiZai <santyagozaidan@gmail.com>
2024-11-22 17:54:36 -0300

    prevent errors & delete reservation functionality

--- lib/data.ts
+++ lib/data.ts
@@ -1,4 +1,4 @@
-import { GET_CONFIG, POST_CONFIG } from './generateConfigs';
+import { DELETE_CONFIG, GET_CONFIG, POST_CONFIG } from './generateConfigs';
 import { Reservation, SPORTS, User } from './models';
 
 /**
@@ -6,11 +6,14 @@ import { Reservation, SPORTS, User } from './models';
  */
 
 const generateFetch = async (url: string, config: {}) => {
-  const data = await fetch(
+  const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_BASE}/${url}`,
     config
   );
-  return data.json();
+  if (!response.ok) {
+    throw new Error(`Error ${response.status}: ${response.statusText}`);
+  }
+  return response.json();
 };
 
 /**
@@ -18,33 +21,33 @@ const generateFetch = async (url: string, config: {}) => {
  */
 
 const getClubsByLocation = async (location: string) =>
-  await generateFetch(`clubs/${location}`, GET_CONFIG);
+  await generateFetch(`clubs/${location}`, GET_CONFIG());
 
 const getClubsByLocationAndSport = async (location: string, sport: SPORTS) =>
-  await generateFetch(`clubs/${location}/${SPORTS[sport]}`, GET_CONFIG);
+  await generateFetch(`clubs/${location}/${SPORTS[sport]}`, GET_CONFIG());
 
 const getClubById = async (id: string) =>
-  generateFetch(`clubs/search/${id}`, GET_CONFIG);
+  generateFetch(`clubs/search/${id}`, GET_CONFIG());
 
 /**
  * COURTS
  */
 
 const getCourtsByClubId = async (clubId: string) =>
-  await generateFetch(`courts/club/${clubId}`, GET_CONFIG);
+  await generateFetch(`courts/club/${clubId}`, GET_CONFIG());
 
 const getCourtById = async (id: string) =>
-  await generateFetch(`courts/${id}`, GET_CONFIG);
+  await generateFetch(`courts/${id}`, GET_CONFIG());
 
 /**
  * USERS
  */
 
 const getUserById = async (id: string) =>
-  await generateFetch(`users/id/${id}`, GET_CONFIG);
+  await generateFetch(`users/id/${id}`, GET_CONFIG());
 
 const getUserByEmail = async (email: string) =>
-  await generateFetch(`users/${email}`, GET_CONFIG);
+  await generateFetch(`users/${email}`, GET_CONFIG());
 
 const createUser = async (user: User) =>
   await generateFetch(
@@ -68,6 +71,9 @@ const createReservation = async (reservation: Partial<Reservation>) =>
     })
   );
 
+const deleteReservation = async (id: string) =>
+  await generateFetch(`reservations/${id}`, DELETE_CONFIG());
+
 /**
  * EXPORTS
  */
@@ -82,4 +88,5 @@ export {
   getUserByEmail,
   createUser,
   createReservation,
+  deleteReservation,
 };
